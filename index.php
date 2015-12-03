1ef4847000dec4d111a7f26f47dcd165f8060674
竹逊 <zhuxun.lx@alibaba-inc.com>
2015-12-03 14:43:28 +0800

    debug

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ if(isset($_GET['game'])) {
 <!DOCTYPE html> 
 <head> 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-	<meta http-equiv="Pragma" content="no-cache" />
-	<meta http-equiv="Expires" content="0" />
     <meta name="renderer" content="webkit">
     <meta name="apple-touch-fullscreen" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes"/>
