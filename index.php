297678be0ba282d100fc9ad80f5855ee32c919d0
竹逊 <zhuxun.lx@alibaba-inc.com>
2015-12-03 14:12:38 +0800

    debug

--- index.php
+++ index.php
@@ -13,20 +13,24 @@ if(isset($_GET['game'])) {
 ?>
 <!DOCTYPE html> 
 <head> 
-	<meta charset="UTF-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
 	<meta http-equiv="Pragma" content="no-cache" />
 	<meta http-equiv="Expires" content="0" />
-	<meta name ="viewport" content ="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="renderer" content="webkit">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	<title><?php echo $game;?></title>
     <script src="phaser.min.js?rand=<?php echo rand(1000,50000);?>"></script>
-    <script>
+    <script type="text/javascript">
 		var GAME = "<?php echo $game;?>";
     </script>
     <?php 
 		include_once 'partials/' . $game . '.php';
 	?>
-    <style>
+    <style type="text/css">
         body {
             margin: 0;
             padding: 0;
