02af7f9508d005a4b18ff4011b96dde608b028db
竹逊 <zhuxun.lx@alibaba-inc.com>
2015-12-03 14:33:25 +0800

    debug

--- index.php
+++ index.php
@@ -23,8 +23,7 @@ if(isset($_GET['game'])) {
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	<title><?php echo $game;?></title>
-    <script src="phaser.min.js?rand=<?php echo rand(1000,50000);?>"></script>
-    <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script src="phaser.min.js"></script>
     <script type="text/javascript">
 		var GAME = "<?php echo $game;?>";
     </script>
