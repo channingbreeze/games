31e616a26b0e52da010b1e4508031fbe3af4e720
竹逊 <zhuxun.lx@alibaba-inc.com>
2016-04-09 16:49:28 +0800

    weidua

--- partials/weiduan/body.php
+++ partials/weiduan/body.php
@@ -2,6 +2,6 @@
   <img id="logo" src="" alt="" />
 </div>
 <div id="share" class="share">
-  <img class="sharepng" src="http://game.webxinxin.com/weiduan/assets/share.png" alt="" />
+  <img class="sharepng" src="http://game.webxinxin.com/weiduan/assets/share.jpg" alt="" />
 </div>
 <div id="game" class="game"></div>
